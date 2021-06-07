48e7cd1b1eb58890f7cdce4b2ee81aea4f8d7225
Celso de Oliveira Silva <celso_os@yahoo.com.br>
2021-06-07 20:26:23 -0300

    Adding Versioning at project

--- 01_RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
+++ 01_RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
@@ -28,6 +28,9 @@ namespace RestWithASP_NET5Udemy
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
+            //Versioning API
+            services.AddApiVersioning();
+
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
